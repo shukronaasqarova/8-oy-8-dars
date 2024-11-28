2d45702f58faa4b5907c6b93bd6d6f86733a4e1e
Shukrona <shukronaasqarova94@gmail.com>
2024-11-28 15:17:31 +0500

    end

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { FC } from 'react'
+import Calendar from './components/Calendar'
+
+const App: FC = () => {
+  return (
+    <div>
+      <Calendar />
+    </div>
+    )
+}
+
+export default App
